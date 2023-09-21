46a3fab3ceffb036b322309c42960654e79522e6
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-09-21 17:41:09 +0200

    built

---
+++ app/loading.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Loading() {
+  return (
+    <main className="text-center">
+        <h2 className="text-primary">
+            Loading...
+        </h2>
+        <p>Hopefully not fro too long...</p>
+    </main>
+  )
+}
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,4 +1,7 @@
+import { Suspense } from "react";
 import TicketsList from "./TicketsList";
+import Loading from "../loading";
+import Link from "next/link";
 
 export default  function Tickets() {
   return (
@@ -8,8 +11,12 @@ export default  function Tickets() {
             <h2>Tickets</h2>
             <p><small>Currently opened tickets </small></p>
           </div>
+          <Link href={`tickets/create`} className="btn-primary p-2 ml-auto">Create new</Link>
         </nav>
-        <TicketsList />
+
+        <Suspense fallback={<Loading/>}>
+          <TicketsList />
+        </Suspense>
     </main>
   )
 }
